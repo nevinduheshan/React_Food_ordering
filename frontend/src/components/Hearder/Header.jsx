1683fa8c36e7a62237d149cf279439e2565f6c38
nevinduheshan <nevinduheshan4@gmail.com>
2024-09-04 17:55:28 +0530

    Navbar and Header build

---
+++ frontend/src/components/Hearder/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order Your Favourite Food Here</h2>
+            <p>Eum per, vestibulum quisque alias aperiam? Eu tincidunt nostra? Tenetur do, nonummy nascetur, sem laborum nostrum, distinctio quisquam ridiculus litora elementum quis, laoreet excepturi, cum maecenas interdum explicabo, earum tempor, corporis nonummy. Labore placerat aspernatur risus, facilisi saepe praesentium dicta. Natus vivamus dolores mollis asperiores egestas viverra quis laborum etiam.</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ frontend/src/pages/Cart/Cart.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Cart.jsx'
+
+const Cart = () => {
+  return (
+    <div>Cart</div>
+  )
+}
+
+export default Cart
