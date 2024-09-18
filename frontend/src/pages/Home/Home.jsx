a4bc42d2c7bf7de25218db3d4a83e4dc4d838d26
nevinduheshan <nevinduheshan4@gmail.com>
2024-09-18 22:12:54 +0530

    Create Food menu

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Home.css'
 import Header from '../../components/Hearder/Header'
+import ExpolreMenu from '../../components/ExploreMenu/ExploreMenu'
 
 const Home = () => {
+
+  const[category,setCategory] = useState("All")
   return (
     <div>
         <Header/>
+        <ExpolreMenu category={category} setCategory={setCategory}/>
     </div>
   )
 }
